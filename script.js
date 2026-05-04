8dc8127882f2f6de3afb1034431bcbc54dd8ee32
rafay-sandhu <88657320+rafay-sandhu@users.noreply.github.com>
2026-05-04 22:47:37 +0500

    Create playRound() and playGame() functions in script.js to simulate rock-paper-scissors rounds. Create global variables humanScore and computerScore to keep track of the players' scores.

--- script.js
+++ script.js
@@ -7,11 +7,10 @@ function getComputerChoice() {
     else return "Paper";
 }
 
-console.log(getComputerChoice());
-
 function getHumanChoice() {
 
     let user_input = prompt("Please enter your choice: ");
+    user_input = user_input.toLowerCase();
 
     if (user_input == "rock") 
         return "Rock";
@@ -20,4 +19,76 @@ function getHumanChoice() {
     else return "Paper";  
 }
 
-console.log(getHumanChoice());
+function playRound(humanChoice, computerChoice) {
+    if (humanChoice == "Rock" && computerChoice == "Rock") {
+        console.log("Human: " + humanChoice);
+        console.log("Computer: " + computerChoice);
+        console.log("It's a tie!");
+    }
+    else if (humanChoice == "Paper" && computerChoice == "Paper") {
+        console.log("Human: " + humanChoice);
+        console.log("Computer: " + computerChoice);
+        console.log("It's a tie!");
+    }
+    else if (humanChoice == "Scissors" && computerChoice == "Scissors") {
+        console.log("Human: " + humanChoice);
+        console.log("Computer: " + computerChoice);
+        console.log("It's a tie!");
+    }
+    else if (humanChoice == "Rock" && computerChoice == "Paper") {
+        console.log("Human: " + humanChoice);
+        console.log("Computer: " + computerChoice);
+        console.log("The computer wins!");
+        computerScore++;
+    }
+    else if (humanChoice == "Rock" && computerChoice == "Scissors") {
+        console.log("Human: " + humanChoice);
+        console.log("Computer: " + computerChoice);
+        console.log("The human wins!");
+        humanScore++;
+    }
+    else if (humanChoice == "Paper" && computerChoice == "Rock") {
+        console.log("Human: " + humanChoice);
+        console.log("Computer: " + computerChoice);
+        console.log("The human wins!");
+        humanScore++;
+    }
+    else if (humanChoice == "Paper" && computerChoice == "Scissors") {
+        console.log("Human: " + humanChoice);
+        console.log("Computer: " + computerChoice);
+        console.log("The computer wins!");
+        computerScore++;
+    }
+    else if (humanChoice == "Scissors" && computerChoice == "Rock") {
+        console.log("Human: " + humanChoice);
+        console.log("Computer: " + computerChoice);
+        console.log("The computer wins!");
+        computerScore++;
+    }
+    else {
+        console.log("Human: " + humanChoice);
+        console.log("Computer: " + computerChoice);
+        console.log("The human wins!");
+        humanScore++;
+    }
+}
+
+humanScore = 0;
+computerScore = 0;
+
+function playGame() {
+    h_score = 0
+    c_score = 0
+
+    playRound(getHumanChoice(), getComputerChoice());
+
+    humanScore += h_score;
+    computerScore += c_score;
+
+    console.log(`Human score: ${humanScore}`);
+    console.log(`Computer score: ${computerScore}`);
+}
+
+for (let i = 0; i < 5; i++){
+    playGame();
+}
